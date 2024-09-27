06c02e503763e8b1740223f0834e196f2c5d3e93
Endrio Maciel <147657790+Endrio-Maciel@users.noreply.github.com>
2024-09-26 21:01:58 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,86 @@
+const main = document.querySelector("main")
+const root = document.querySelector(":root")
+const input = document.getElementById("input")
+const resultInput = document.getElementById("result")
+const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
+
+// função para os botões 
+document.querySelectorAll('.charKey').forEach(function(charKeyBtn){
+    charKeyBtn.addEventListener('click', function(){
+        const value = charKeyBtn.dataset.value
+        input.value += value
+    })
+})
+
+document.getElementById('clear').addEventListener('click', function (){
+
+    input.value = ''
+    input.focus()
+    
+})
+
+
+input.addEventListener('keydown', function(ev){
+
+    //função para verificar as teclas pressionadas, para apenas número e caracteres da calculadora. 
+    ev.preventDefault()
+    if (allowedKeys.includes(ev.key)){
+        input.value += ev.key
+        return
+    }    
+   
+    // Tecla backspace apaga o último caracter do input da calculadora.
+    if (ev.key === 'Backspace') {
+        input.value = input.value.slice(0, -1)
+    }   
+    if (ev.key === 'Enter'){
+        calculate()
+    }
+
+})
+document.getElementById('equal').addEventListener('click', calculate)
+function calculate() {
+    resultInput.value = "ERROR"
+    resultInput.classList.add("error")
+    const result = eval(input.value)
+    resultInput.value = result
+    resultInput.classList.remove("error")
+  }
+
+document.getElementById('copyToClipboard').addEventListener('click' , function(ev){
+        // quem aciona o evento, que é o próprio botão
+    const button = ev.currentTarget
+    if (button.innerText === "Copy" ){
+        button.innerText = "Copied!"
+        button.classList.add('success')
+        window.navigator.clipboard.writeText(resultInput.value)
+    } else {
+        button.innerText = "Copy"
+        button.classList.remove('success')
+    }
+
+
+})
+
+document.getElementById('clearResult').addEventListener('click', function(){
+
+    resultInput.value = ''
+    resultInput.classList.remove("error")
+})
+
+
+document.getElementById('themeSwitcher').addEventListener('click', function(){
+    if (main.dataset.theme === 'dark') {
+        root.style.setProperty('--bg-color', '#f1f5f9')
+        root.style.setProperty('--border-color', '#aaa')
+        root.style.setProperty('--font-color', '#212529')
+        root.style.setProperty('--primary-color', '#268334')
+        main.dataset.theme = 'light'
+    } else {
+        root.style.setProperty('--bg-color', '#212529')
+        root.style.setProperty('--border-color', '#666')
+        root.style.setProperty('--font-color', '#f1f5f9')
+        root.style.setProperty('--primary-color', '#4dff91')
+        main.dataset.theme = 'dark'
+    }
+})
